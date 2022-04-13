0ad49ac4cdada211221d662b90555dd604ae186e
Artem <31787423+DokSna@users.noreply.github.com>
2022-04-13 20:41:26 +0300

    Добавил "const" & "@override", удалил комментарии.

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,24 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.indigo[300],
         appBar: AppBar(
-          title: Text('Counter'),
+          title: const Text('Counter'),
           centerTitle: true,
           backgroundColor: Colors.indigo,
         ),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
+            children: const <Widget>[
               Text('Tap "-" to decrement',
                   style: TextStyle(color: Colors.white, fontSize: 16)),
               CounterWidget(),
@@ -31,6 +33,8 @@ class MyApp extends StatelessWidget {
 }
 
 class CounterWidget extends StatefulWidget {
+  const CounterWidget({Key? key}) : super(key: key);
+
   @override
   _CounterWidgetState createState() => _CounterWidgetState();
 }
@@ -38,17 +42,14 @@ class CounterWidget extends StatefulWidget {
 class _CounterWidgetState extends State<CounterWidget> {
   int _count = 50;
 
+  @override
   Widget build(BuildContext context) {
     return Container(
-      // alignment: Alignment.center,
-      // color: Colors.indigo[100],
-      // padding: const EdgeInsets.all(16),
-      margin: EdgeInsets.all(5),
+      margin: const EdgeInsets.all(5),
       width: 120.0,
       height: 50.0,
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(10.0),
-        // shape: BoxShape.circle,
         color: Colors.indigo[50],
       ),
       child: Row(
@@ -60,21 +61,18 @@ class _CounterWidgetState extends State<CounterWidget> {
                   _count -= 1;
                 });
               },
-              icon: Icon(Icons.remove)),
+              icon: const Icon(Icons.remove)),
           Text(
             '$_count',
-            style: TextStyle(fontSize: 20),
+            style: const TextStyle(fontSize: 20),
           ),
           IconButton(
               onPressed: () {
                 setState(() => _count += 1);
               },
-              icon: Icon(Icons.add)),
+              icon: const Icon(Icons.add)),
         ],
       ),
-      // decoration: BoxDecoration(
-      //     // shape: BoxShape.circle
-      //     ),
     );
   }
 }
